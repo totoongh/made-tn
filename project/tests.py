57bb3585403c00104d8f2263a8e21c2e2a906abc
Thomas Neher <thomas.neher@hotmail.de>
2023-12-06 17:04:39 +0100

    Start action run

--- project/tests.py
+++ project/tests.py
@@ -4,4 +4,4 @@ print(f"\tLooking for project.sqlite database.")
 if os.path.isfile('project.sqlite'):
     print(f"\t[SUCCESS] Found project.sqlite database.")
 else:
-    print(f"\t[ERROR]Could not find it.")
+    print(f"\t[ERROR]Could not find it. ")
